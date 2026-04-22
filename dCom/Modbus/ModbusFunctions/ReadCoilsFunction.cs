759bbbb2f6e91c8b3149b4e23099cfc711af5163
roksandicm <roxa4400@gmail.com>
2026-04-22 17:39:08 +0200

    ReadCoilsGotov

--- dCom/Modbus/ModbusFunctions/ReadCoilsFunction.cs
+++ dCom/Modbus/ModbusFunctions/ReadCoilsFunction.cs
@@ -24,15 +24,57 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc/>
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusReadCommandParameters p = (ModbusReadCommandParameters)CommandParameters;
+            byte[] request = new byte[12];
+
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)p.TransactionId)), 0, request, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)p.ProtocolId)), 0, request, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)p.Length)), 0, request, 4, 2);
+            request[6] = p.UnitId;
+            request[7] = p.FunctionCode;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)p.StartAddress)), 0, request, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)p.Quantity)), 0, request, 10, 2);
+
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            Dictionary<Tuple<PointType, ushort>, ushort> result = new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            if (response[7] == (byte)(CommandParameters.FunctionCode + 0x80))
+            {
+                HandeException(response[8]);
+            }
+
+            ModbusReadCommandParameters readParameters = (ModbusReadCommandParameters)CommandParameters;
+            ushort startAddress = readParameters.StartAddress;
+            int count = 0;
+            int byteCount = response[8];
+            ushort mask = 1;
+
+            for (int i = 0; i < byteCount; i++)
+            {
+                byte temp = response[9 + i];
+
+                for (int j = 0; j < 8; j++)
+                {
+                    ushort value = (ushort)((temp & mask) != 0 ? 1 : 0);
+                    result.Add(new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, startAddress), value);
+
+                    temp >>= 1;
+                    count++;
+                    startAddress++;
+
+                    if (count >= readParameters.Quantity)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
